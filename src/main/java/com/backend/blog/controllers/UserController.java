48ad0d7ab764c09baaaa6133371c2e2cecd1dde2
vivxk <viveksankhla222@gmail.com>
2022-04-21 19:43:31 +0530

    Files added

--- src/main/java/com/backend/blog/controllers/UserController.java
+++ src/main/java/com/backend/blog/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.backend.blog.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,13 +28,13 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
 		UserDto createdUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable Integer userId) {
 		UserDto updatedUser = this.userService.UpdateUser(userDto, userId);
 		return ResponseEntity.ok(updatedUser);
 	}
--- src/main/java/com/backend/blog/payloads/UserDto.java
+++ src/main/java/com/backend/blog/payloads/UserDto.java
@@ -1,5 +1,11 @@
 package com.backend.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,10 +14,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+
 	private int id;
+	@NotEmpty(message = "Username cannot be empty!")
+	@Size(min = 4, max = 20, message = "Username must be between 4 to 20 characters!")
 	private String name;
+
+	@Email(message = "Invalid Email address!")
 	private String email;
+	@NotEmpty(message = "Password cannot be blank!")
+	@Size(min = 8, max = 16, message = "Password must be between 8 to 16 characters!")
 	private String password;
+	@NotEmpty
 	private String about;
 
 }
