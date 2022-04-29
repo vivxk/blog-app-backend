46d6f9f6aad02aefb88504bda83680d6be931bdc
vivxk <viveksankhla222@gmail.com>
2022-04-29 10:47:25 +0530

    Implementing searching and sorting in post

--- src/main/java/com/backend/blog/repositories/PostRepo.java
+++ src/main/java/com/backend/blog/repositories/PostRepo.java
@@ -16,12 +16,12 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
 
 	List<Post> findByCategory(Category category);
 
-	@Query("select p from Post p where p.title like :key") //using manual query 
+	@Query("select p from Post p where p.title like :key") // using manual query
 	List<Post> findByTitleContaining(@Param("key") String title);
 
 	// List<Post> findByTitleContaining(String title);
 	/*
-	 * not being used currently due to bug in hibernate 5.6.7 version which return
+	 * not being used currently due to bug in hibernate 5.6.7 version Which returns
 	 * error when searching for the second time
 	 */
 }
